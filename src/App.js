55dd3aa807495f1b02b8f260d41bebf5ab279fbb
Adolphus, Amarachi Victoria <111003235+Sardonyc95@users.noreply.github.com>
2023-08-13 19:51:22 +0100

    Integrated the CSS styling

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
       <div className="container">
         <Weather />
         <footer>
-          This project is coded by Amavic.{" "}
+          This project is coded by <a href='https://fascinating-chimera-f34764.netlify.app/' rel='noreferrer' target='_blank'>Amavic</a> and is{" "}
           <a href="https://github.com/Sardonyc95/SheCodes.io" rel="noreferrer" target="_blank">
-            Open source on github
+            Open source on GitHub
           </a>
         </footer>
       </div>
--- src/Weather.js
+++ src/Weather.js
@@ -4,18 +4,25 @@ export default function Weather(){
     return(
         <div className="Weather">
             <form>
-                <input type="search" placeholder='Enter a city...' className='formInput' />
-                <input type='submit' value='Search' className='formSubmit' />
+                <div className='row'>
+                    <div className='col-9'>
+                        <input type="search" placeholder='Enter a city...' className='formSearch' autoFocus='on' />
+                    </div>
+                    <div className='col-3'>
+                        <input type='submit' value='Search' className='btn btn-primary w-100' />
+                    </div>
+                </div>
             </form>
             <h1>New York</h1>
             <ul>
                 <li>Sunday 05:20</li>
-                <li>Mostly cloudy</li>
+                <li>Expect light rain</li>
             </ul>
-            <div className='row'>
+            <div className='row mt-3'>
                 <div className='col-6'>
                     <img src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png" alt="Mostly cloudy"/>
-                    25°C|°F
+                    <span className='temperature'>28</span>
+                    <span className='celsuisUnit'>°C |</span>{" "}<span className='fahrenheitUnit'>°F</span>
                 </div>
                 <div className='col-6'>
                     <ul>
