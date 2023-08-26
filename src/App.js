84a84efbd014b222386971a5e819d8b6d7b7a92c
Adolphus, Amarachi Victoria <111003235+Sardonyc95@users.noreply.github.com>
2023-08-26 14:13:26 +0100

    Minor changes to the project links

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Owerri"/>
         <footer>
-          This project is coded by <a href='https://fascinating-chimera-f34764.netlify.app/' rel='noreferrer' target='_blank'>Amavic</a> and is{" "}
+          This project is coded by 
+          <a href='https://fascinating-chimera-f34764.netlify.app/' rel='noreferrer' target='_blank'>Amavic</a> and is{" "}
           <a href="https://github.com/Sardonyc95/SheCodes.io" rel="noreferrer" target="_blank">
-            Open source on GitHub and <a href='https://fascinating-chimera-f34764.netlify.app/' rel="noreferrer" target="_blank">hosted on Netlify</a>
-          </a>
+            Open source on GitHub and </a>
+          <a href='https://fascinating-chimera-f34764.netlify.app/' rel="noreferrer" target="_blank">hosted on Netlify</a>
         </footer>
       </div>
     </div>
